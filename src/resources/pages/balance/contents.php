e82c7767eb10c7f9d2cf36269f8a49f1f5718939
Netkas <45276434+Netkas@users.noreply.github.com>
2019-08-11 16:27:12 -0400

    Created /balance

---
+++ src/resources/pages/balance/contents.php
@@ -0,0 +1,35 @@
+<?PHP
+    use DynamicalWeb\HTML;
+
+    $UsernameSafe = ucfirst(WEB_ACCOUNT_USERNAME);
+    if(strlen($UsernameSafe) > 16)
+    {
+        $UsernameSafe = substr($UsernameSafe, 0 ,16);
+        $UsernameSafe .= "...";
+    }
+?>
+<!doctype html>
+<html lang="<?PHP HTML::print(APP_LANGUAGE_ISO_639); ?>">
+    <head>
+        <?PHP HTML::importSection('dashboard_headers'); ?>
+        <title>Intellivoid Accounts</title>
+    </head>
+
+    <body>
+        <div class="container-scroller">
+            <?PHP HTML::importSection("dashboard_navbar"); ?>
+            <div class="container-fluid page-body-wrapper">
+                <div class="main-panel container">
+                    <div class="content-wrapper">
+
+                      <h4>This feature is coming soon!</h4>
+
+                    </div>
+                    <?PHP HTML::importSection('dashboard_footer'); ?>
+                </div>
+            </div>
+
+        </div>
+        <?PHP HTML::importSection('dashboard_js'); ?>
+    </body>
+</html>
