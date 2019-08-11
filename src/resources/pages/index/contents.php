c11597bbd74cbdb300649abf824e56de88add542
Netkas <45276434+Netkas@users.noreply.github.com>
2019-08-11 11:39:17 -0400

    Added missing js for navigation bar in mobile mode

--- src/resources/pages/index/contents.php
+++ src/resources/pages/index/contents.php
@@ -1,6 +1,12 @@
 <?PHP
     use DynamicalWeb\HTML;
-    HTML::importScript('auth.register');
+
+    $UsernameSafe = ucfirst(WEB_ACCOUNT_USERNAME);
+    if(strlen($UsernameSafe) > 16)
+    {
+        $UsernameSafe = substr($UsernameSafe, 0 ,16);
+        $UsernameSafe .= "...";
+    }
 ?>
 <!doctype html>
 <html lang="<?PHP HTML::print(APP_LANGUAGE_ISO_639); ?>">
@@ -15,6 +21,21 @@
             <div class="container-fluid page-body-wrapper">
                 <div class="main-panel container">
                     <div class="content-wrapper">
+
+                        <div class="row">
+                            <div class="col-12 grid-margin d-none d-lg-block">
+                                <div class="intro-banner">
+                                    <div class="banner-image">
+                                        <img src="../assets/images/dashboard/banner_img.png" alt="banner image"> </div>
+                                    <div class="content-area">
+                                        <h3 class="mb-0">Welcome back, <?PHP HTML::print($UsernameSafe); ?>!</h3>
+                                        <p class="mb-0">Need anything more to know more? Feel free to contact us at any point.</p>
+                                    </div>
+                                    <a href="https://intellivoid.info/contact" target="_blank" class="btn btn-light">Contact us</a>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                     <?PHP HTML::importSection('dashboard_footer'); ?>
                 </div>
--- src/resources/sections/dashboard_navbar.php
+++ src/resources/sections/dashboard_navbar.php
@@ -1,105 +1,26 @@
+<?PHP
+
+    use DynamicalWeb\HTML;
+
+    $UsernameSafe = ucfirst(WEB_ACCOUNT_USERNAME);
+    if(strlen($UsernameSafe) > 16)
+    {
+        $UsernameSafe = substr($UsernameSafe, 0 ,16);
+        $UsernameSafe .= "...";
+    }
+?>
 <nav class="navbar horizontal-layout col-lg-12 col-12 p-0">
     <div class="container d-flex flex-row nav-top">
         <div class="text-center navbar-brand-wrapper d-flex align-items-top">
             <a class="navbar-brand brand-logo" href="index.html">
-                <img src="../assets/images/logo_2.svg" alt="logo" /> </a>
+                <img src="../assets/images/logo_2.png" alt="logo" /> </a>
             <a class="navbar-brand brand-logo-mini" href="index.html">
-                <img src="../assets/images/logo-mini.svg" alt="logo" /> </a>
+                <img src="../assets/images/iv_logo.svg" alt="logo" /> </a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center">
-            <form action="form-action" class="d-none d-sm-block">
-                <div class="input-group search-box">
-                    <div class="input-group-prepend">
-                <span class="input-group-text">
-                  <i class="mdi mdi mdi-magnify"></i>
-                </span>
-                    </div>
-                    <input type="text" class="form-control" placeholder="Type to search…">
-                    <i class="mdi mdi mdi-close search-close"></i>
-                </div>
-            </form>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item dropdown">
-                    <a class="nav-link count-indicator dropdown-toggle" id="messageDropdown" href="#" data-toggle="dropdown"
-                       aria-expanded="false">
-                        <i class="mdi mdi-file-outline"></i>
-                        <span class="count">7</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list pb-0" aria-labelledby="messageDropdown">
-                        <a class="dropdown-item py-3">
-                            <p class="mb-0 font-weight-medium float-left">You have 7 unread mails </p>
-                            <span class="badge badge-pill badge-primary float-right">View all</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item preview-item">
-                            <div class="preview-thumbnail">
-                                <img src="../assets/images/faces/face10.jpg" alt="image" class="img-sm profile-pic"> </div>
-                            <div class="preview-item-content flex-grow py-2">
-                                <p class="preview-subject ellipsis font-weight-medium text-dark">Marian Garner </p>
-                                <p class="font-weight-light small-text"> The meeting is cancelled </p>
-                            </div>
-                        </a>
-                        <a class="dropdown-item preview-item">
-                            <div class="preview-thumbnail">
-                                <img src="../assets/images/faces/face12.jpg" alt="image" class="img-sm profile-pic"> </div>
-                            <div class="preview-item-content flex-grow py-2">
-                                <p class="preview-subject ellipsis font-weight-medium text-dark">David Grey </p>
-                                <p class="font-weight-light small-text"> The meeting is cancelled </p>
-                            </div>
-                        </a>
-                        <a class="dropdown-item preview-item">
-                            <div class="preview-thumbnail">
-                                <img src="../assets/images/faces/face1.jpg" alt="image" class="img-sm profile-pic"> </div>
-                            <div class="preview-item-content flex-grow py-2">
-                                <p class="preview-subject ellipsis font-weight-medium text-dark">Travis Jenkins </p>
-                                <p class="font-weight-light small-text"> The meeting is cancelled </p>
-                            </div>
-                        </a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown ml-4">
-                    <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
-                        <i class="mdi mdi-bell-outline"></i>
-                        <span class="count bg-success">4</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list pb-0" aria-labelledby="notificationDropdown">
-                        <a class="dropdown-item py-3 border-bottom">
-                            <p class="mb-0 font-weight-medium float-left">You have 4 new notifications </p>
-                            <span class="badge badge-pill badge-primary float-right">View all</span>
-                        </a>
-                        <a class="dropdown-item preview-item py-3">
-                            <div class="preview-thumbnail">
-                                <i class="mdi mdi-alert m-auto text-primary"></i>
-                            </div>
-                            <div class="preview-item-content">
-                                <h6 class="preview-subject font-weight-normal text-dark mb-1">Application Error</h6>
-                                <p class="font-weight-light small-text mb-0"> Just now </p>
-                            </div>
-                        </a>
-                        <a class="dropdown-item preview-item py-3">
-                            <div class="preview-thumbnail">
-                                <i class="mdi mdi-settings m-auto text-primary"></i>
-                            </div>
-                            <div class="preview-item-content">
-                                <h6 class="preview-subject font-weight-normal text-dark mb-1">Settings</h6>
-                                <p class="font-weight-light small-text mb-0"> Private message </p>
-                            </div>
-                        </a>
-                        <a class="dropdown-item preview-item py-3">
-                            <div class="preview-thumbnail">
-                                <i class="mdi mdi-airballoon m-auto text-primary"></i>
-                            </div>
-                            <div class="preview-item-content">
-                                <h6 class="preview-subject font-weight-normal text-dark mb-1">New user registration</h6>
-                                <p class="font-weight-light small-text mb-0"> 2 days ago </p>
-                            </div>
-                        </a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown d-none d-xl-inline-block">
-                    <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">Hello,
-                        Stephen
-                        <img class="img-xs rounded-circle ml-3" src="../assets/images/faces/face8.jpg" alt="Profile image"> </a>
+                    <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false"><?PHP HTML::print($UsernameSafe); ?></a>
                     <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
                         <a class="dropdown-item p-0">
                             <div class="d-flex border-bottom">
@@ -121,12 +42,12 @@
                     </div>
                 </li>
             </ul>
-            <button class="navbar-toggler align-self-center" type="button" data-toggle="minimize">
+            <button class="navbar-toggler align-self-center" id="toggle-navbar-mini" name="toggle-navbar-mini"  type="button" data-toggle="minimize">
                 <span class="mdi mdi-menu"></span>
             </button>
         </div>
     </div>
-    <div class="nav-bottom">
+    <div class="nav-bottom" id="navigation_bar" name="navigation_bar">
         <div class="container">
             <ul class="nav page-navigation">
                 <li class="nav-item">
