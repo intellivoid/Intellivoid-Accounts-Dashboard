7e39991cb0b17639815b646ee40b5f3c849fb6ac
Netkas <45276434+Netkas@users.noreply.github.com>
2019-08-11 02:20:39 -0400

    Added Dashboard assets

--- src/resources/pages/index/contents.php
+++ src/resources/pages/index/contents.php
@@ -5,28 +5,22 @@
 <!doctype html>
 <html lang="<?PHP HTML::print(APP_LANGUAGE_ISO_639); ?>">
     <head>
-        <?PHP HTML::importSection('headers'); ?>
+        <?PHP HTML::importSection('dashboard_headers'); ?>
         <title>Intellivoid Accounts</title>
     </head>
 
     <body>
         <div class="container-scroller">
-            <div class="container-fluid page-body-wrapper full-page-wrapper">
-                <div class="content-wrapper d-flex align-items-center auth auth-bg-1 theme-one">
-                    <div class="row w-100 mx-auto">
-                        <div class="col-lg-4 mx-auto">
-                            <div class="auto-form-wrapper">
-                                <h1 class="text-center">
-                                    <img src="/assets/images/iv_logo.svg" alt="Intellivoid Blue Logo" class="img-sm rounded-circle"/>
-                                    Intelli<b>void</b>
-                                </h1>
-                                <div class="pb-4"></div>
-                            </div>
-                        </div>
+            <?PHP HTML::importSection("dashboard_navbar"); ?>
+            <div class="container-fluid page-body-wrapper">
+                <div class="main-panel container">
+                    <div class="content-wrapper">
                     </div>
+                    <?PHP HTML::importSection('dashboard_footer'); ?>
                 </div>
             </div>
+
         </div>
-        <?PHP HTML::importSection('js_scripts'); ?>
+        <?PHP HTML::importSection('dashboard_js'); ?>
     </body>
 </html>
---
+++ src/resources/sections/dashboard_js.php
@@ -0,0 +1,7 @@
+<script src="/assets/vendors/js/vendor.bundle.base.js"></script>
+<script src="/assets/vendors/js/vendor.bundle.addons.js"></script>
+<script src="/assets/js/shared/off-canvas.js"></script>
+<script src="/assets/js/shared/hoverable-collapse.js"></script>
+<script src="/assets/js/shared/misc.js"></script>
+<script src="/assets/js/shared/settings.js"></script>
+<script src="/assets/js/shared/todolist.js"></script>
