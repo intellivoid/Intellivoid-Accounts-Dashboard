a16feeb01bbfa0ae1774e903ae0a4bec5b2ee811
Netkas <netkas@users.noreply.github.com>
2019-09-07 19:15:13 -0400

    Added authentication logs for /verify_mobile

--- src/resources/pages/verify_mobile/scripts/verify_code.php
+++ src/resources/pages/verify_mobile/scripts/verify_code.php
@@ -1,8 +1,16 @@
 <?php
 
     use DynamicalWeb\DynamicalWeb;
-    use IntellivoidAccounts\Objects\Account;
-    use sws\sws;
+use IntellivoidAccounts\Abstracts\LoginStatus;
+use IntellivoidAccounts\Abstracts\SearchMethods\KnownHostsSearchMethod;
+use IntellivoidAccounts\Exceptions\DatabaseException;
+use IntellivoidAccounts\Exceptions\HostNotKnownException;
+use IntellivoidAccounts\Exceptions\InvalidIpException;
+use IntellivoidAccounts\IntellivoidAccounts;
+use IntellivoidAccounts\Objects\Account;
+use IntellivoidAccounts\Objects\KnownHost;
+use sws\Objects\Cookie;
+use sws\sws;
 
     if(isset($_GET['action']))
     {
@@ -15,6 +23,26 @@
         }
     }
 
+    /**
+     * Get's the known host associated with this client
+     *
+     * @return KnownHost
+     * @throws DatabaseException
+     * @throws HostNotKnownException
+     * @throws InvalidIpException
+     */
+    function get_host(): KnownHost
+    {
+        /** @var IntellivoidAccounts $IntellivoidAccounts */
+        $IntellivoidAccounts = DynamicalWeb::getMemoryObject("intellivoid_accounts");
+
+        /** @var Cookie $Cookie */
+        $Cookie = DynamicalWeb::getMemoryObject('(cookie)web_session');
+
+        return $IntellivoidAccounts->getKnownHostsManager()->getHost(KnownHostsSearchMethod::byId, $Cookie->Data['host_id']);
+    }
+
+
     function execute_verification()
     {
         if(isset($_POST['code']) == false)
@@ -26,9 +54,19 @@
         /** @var Account $Account */
         $Account = DynamicalWeb::getMemoryObject('account');
 
+        $Host = get_host();
+
+        /** @var IntellivoidAccounts $IntellivoidAccounts */
+        $IntellivoidAccounts = DynamicalWeb::getMemoryObject("intellivoid_accounts");
+
         if($Account->Configuration->VerificationMethods->TwoFactorAuthentication->verifyCode($_POST['code']) == false)
         {
-            // TODO: Log incorrect verification code here.
+            $IntellivoidAccounts->getLoginRecordManager()->createLoginRecord(
+                $Account->ID, $Host->ID,
+                LoginStatus::VerificationFailed, 'Intellivoid Accounts',
+                CLIENT_USER_AGENT
+            );
+
             // TODO: Add auto-lockout
             header('Location: /verify_mobile?callback=101&incorrect_auth=1');
             exit();
@@ -43,6 +81,12 @@
         $Cookie->Data["verification_attempts"] = 0;
 
         $sws->CookieManager()->updateCookie($Cookie);
+
+        $IntellivoidAccounts->getLoginRecordManager()->createLoginRecord(
+            $Account->ID, $Host->ID,
+            LoginStatus::Successful, 'Intellivoid Accounts',
+            CLIENT_USER_AGENT
+        );
 
         header('Location: /');
         exit();
