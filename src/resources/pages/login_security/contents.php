2477d54306739cf6aa568b8f769917d3fd03a91f
Netkas <45276434+Netkas@users.noreply.github.com>
2019-11-02 13:03:01 -0300

    Added res dialog.disable-rc

--- src/resources/pages/login_security/contents.php
+++ src/resources/pages/login_security/contents.php
@@ -213,32 +213,8 @@ use IntellivoidAccounts\Objects\Account;
                                 </div>
                             </div>
                             <?PHP HTML::importScript('dialog.disable-rc-mv'); ?>
-                            <div class="modal fade" id="disable-rc" tabindex="-1" role="dialog" aria-labelledby="disable-rc-label" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="disable-rc-label">Disable Recovery Codes</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">
-                                                    <i class="mdi mdi-close"></i>
-                                                </span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <p>
-                                                Please confirm that you want to disable recovery codes. Your old recovery
-                                                codes will become invalid and you will  no longer be prompted for a
-                                                recovery code when trying to verify  your login
-                                            </p>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <?PHP $Href = DynamicalWeb::getRoute('login_security', array('action' => 'disable_rc')); ?>
-                                            <button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
-                                            <button type="button" class="btn btn-danger" onclick="location.href='<?PHP HTML::print($Href); ?>';">Disable</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <?PHP HTML::importScript('dialog.disable-rc'); ?>
+
                             <div class="modal fade" id="unlink-telegram" tabindex="-1" role="dialog" aria-labelledby="unlink-telegram-label" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
