dec518a527ff046da54e3b0968d22b22db51091c
netkas <netkas@intellivoid.info>
2020-02-05 12:38:42 -0300

    Minor correct for secured web sessions

--- src/resources/libraries/sws/AutoConfig.php
+++ src/resources/libraries/sws/AutoConfig.php
@@ -8,7 +8,7 @@
         include_once(__DIR__ . DIRECTORY_SEPARATOR . 'acm' . DIRECTORY_SEPARATOR . 'acm.php');
     }
 
-    $acm = new acm(__DIR__, 'sws');
+    $acm = new acm(__DIR__, 'intellivoid_accounts');
 
     $DatabaseSchema = new Schema();
     $DatabaseSchema->setDefinition('Host', 'localhost');
