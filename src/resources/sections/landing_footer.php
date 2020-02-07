e6f7ce2a9c19e0228a97833ffb3ce9515bddbadb
netkas <netkas@intellivoid.info>
2020-02-07 10:54:40 -0300

    Corrected formatting on the landing_footer section

--- src/resources/sections/landing_footer.php
+++ src/resources/sections/landing_footer.php
@@ -3,9 +3,9 @@
 ?>
 <footer class="container-fluid footer">
     <div class="row pb-5 text-white footer-top">
-        <div class="col-md-8 mx-auto">
+        <div class="mx-auto">
             <div class="row">
-                <div class="col-md-8 mt-auto mb-4 ml-5">
+                <div class="mt-auto mb-4 ml-5">
                     <ul class="nav mt-3">
                         <li class="nav-item">
                             <a class="nav-link" href="https://intellivoid.info/privacy">Privacy Policies</a>
