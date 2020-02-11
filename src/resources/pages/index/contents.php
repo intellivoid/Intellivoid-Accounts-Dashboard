535953399e2210d8c1b540d914a4b9459fbf8bae
netkas <netkas@intellivoid.info>
2020-02-11 11:47:43 -0300

    Added missing routes

--- src/resources/pages/index/contents.php
+++ src/resources/pages/index/contents.php
@@ -62,8 +62,29 @@ use IntellivoidAccounts\IntellivoidAccounts;
                                 </div>
                             </div>
                         </div>
+                        <?PHP
+                            if($Account->Configuration->VerificationMethods->TelegramClientLinked == false)
+                            {
+                                ?>
+                                <div class="alert alert-fill-light border-dark" role="alert">
+                                    <h4 class="text-black mb-3">
+                                        <i class="mdi mdi-24px mdi-lock-plus text-black"></i> Account Security
+                                    </h4>
+                                    <span class="text-black">
+                                        It is important that you link your Telegram Account with your Intellivoid Account, this will
+                                        provide greater security to your Account and allow you to recover your account in the case you
+                                        lose access to it
+                                    </span>
+                                    <div class="mt-4 mb-2">
+                                        <button class="btn btn-primary">
+                                            <i class="mdi mdi-telegram pr-1"></i>Open Telegram
+                                        </button>
+                                    </div>
+                                </div>
+                                <?PHP
+                            }
+                        ?>
                         <div class="row">
-
                             <div class="col-md-4 grid-margin stretch-card">
                                 <div class="card">
                                     <div class="card-header header-sm d-flex justify-content-between align-items-center">
@@ -73,7 +94,7 @@ use IntellivoidAccounts\IntellivoidAccounts;
                                                 <i class="mdi mdi-dots-vertical"></i>
                                             </button>
                                             <div class="dropdown-menu" aria-labelledby="recentActionsDropDown">
-                                                <a class="dropdown-item" href="<?PHP \DynamicalWeb\DynamicalWeb::getRoute('audit_logs', array(), true); ?>">View More</a>
+                                                <a class="dropdown-item" href="<?PHP DynamicalWeb::getRoute('audit_logs', array(), true); ?>">View More</a>
                                             </div>
                                         </div>
                                     </div>
@@ -90,24 +111,21 @@ use IntellivoidAccounts\IntellivoidAccounts;
                                     <div class="card-body">
                                         <div class="row">
                                             <div class="col-md-6 d-flex flex-column mb-4">
-                                                <div class="d-flex align-items-center pb-3">
-                                                    <h4 class="card-title mb-0">Overview</h4>
-                                                </div>
+                                                <small class="text-muted d-none d-lg-block mb-3">Overview</small>
                                                 <div class="mb-3">
-                                                    <p class="text-muted mb-2">Account Balance</p>
                                                     <div class="d-flex align-items-center">
                                                         <h1 class="font-weight-medium mb-2">$<?PHP HTML::print($Account->Configuration->Balance); ?> USD</h1>
                                                     </div>
                                                     <div class="d-flex align-items-center">
                                                         <p class="text-muted mb-0 ml-1">
-                                                            <a class="text-primary" href="#">Manage Account Balance</a>
+                                                            <a class="text-primary" href="<?PHP DynamicalWeb::getRoute('account_balance', array(), true); ?>">Manage Account Balance</a>
                                                         </p>
                                                     </div>
                                                 </div>
                                                 <div class="border-top"></div>
                                                 <div class="d-flex justify-content-between py-1 mt-2 mb-0">
                                                     <div class="wrapper">
-                                                        <a class="mb-0 text-small text-black" href="#" style="text-decoration: none;">
+                                                        <a class="mb-0 text-small text-black" href="<?PHP DynamicalWeb::getRoute('services', array(), true); ?>" style="text-decoration: none;">
                                                             <i class="mdi mdi-account-key pr-2"></i>
                                                             Manage Authorized Applications
                                                         </a>
@@ -115,7 +133,7 @@ use IntellivoidAccounts\IntellivoidAccounts;
                                                 </div>
                                                 <div class="d-flex justify-content-between py-1 mt-0 mb-0">
                                                     <div class="wrapper">
-                                                        <a class="mb-0 text-small text-black" href="#" style="text-decoration: none;">
+                                                        <a class="mb-0 text-small text-black" href="<?PHP DynamicalWeb::getRoute('applications', array(), true); ?>" style="text-decoration: none;">
                                                             <i class="mdi mdi-application pr-2"></i>
                                                             Manage Applications
                                                         </a>
@@ -123,7 +141,7 @@ use IntellivoidAccounts\IntellivoidAccounts;
                                                 </div>
                                                 <div class="d-flex justify-content-between py-1">
                                                     <div class="wrapper">
-                                                        <a class="mb-0 text-small text-black" href="#" style="text-decoration: none;">
+                                                        <a class="mb-0 text-small text-black" href="<?PHP DynamicalWeb::getRoute('login_history', array(), true); ?>" style="text-decoration: none;">
                                                             <i class="mdi mdi-history pr-2"></i>
                                                             View Login History
                                                         </a>
@@ -242,7 +260,7 @@ use IntellivoidAccounts\IntellivoidAccounts;
                                                     </div>
                                                 </div>
                                                 <div class="wrapper mt-4 d-flex d-lg-block">
-                                                    <button class="btn btn-danger btn-block">
+                                                    <button class="btn btn-danger btn-block" onclick="location.href='<?PHP DynamicalWeb::getRoute('login_security', array(), true); ?>';">
                                                         <i class="mdi mdi-lock pr-1"></i>Manage Login Security
                                                     </button>
                                                 </div>
