70437202c45bc31b789e0bf923a855b2e1524dbf
netkas <netkas@intellivoid.info>
2020-02-12 23:44:34 -0300

    Bug fix in json_response.go

--- src/resources/shared/scripts/json_response.php
+++ src/resources/shared/scripts/json_response.php
@@ -3,9 +3,9 @@
     function returnJsonResponse(array $payload)
     {
         $response = json_encode($payload);
-        if(isset($payload['status_code']))
+        if(isset($payload['response_code']))
         {
-            http_response_code($payload['status_code']);
+            http_response_code($payload['response_code']);
         }
         header('Content-Type: application/json');
         header('Content-Length: ' . strlen($response));
