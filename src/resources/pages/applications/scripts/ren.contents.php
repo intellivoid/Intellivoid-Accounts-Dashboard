da1a86c914df1ad653f9550287cf00b3cfcd40f2
netkas <netkas@intellivoid.info>
2020-02-12 22:50:48 -0300

    Added translation for 'applications'

--- src/resources/pages/applications/scripts/ren.contents.php
+++ src/resources/pages/applications/scripts/ren.contents.php
@@ -2,7 +2,7 @@
 
     use DynamicalWeb\DynamicalWeb;
     use DynamicalWeb\HTML;
-use IntellivoidAccounts\Abstracts\ApplicationStatus;
+    use IntellivoidAccounts\Abstracts\ApplicationStatus;
 
     function render_items(array $items)
     {
@@ -12,17 +12,16 @@ use IntellivoidAccounts\Abstracts\ApplicationStatus;
             ?>
             <div class="list-item">
                 <div class="preview-image">
-                    <img class="img-sm" src="<?PHP DynamicalWeb::getRoute('application_icon', array('app_id' => $application['public_app_id'], 'resource' => 'small'), true); ?>" alt="profile image">
+                    <img class="img-sm" src="<?PHP DynamicalWeb::getRoute('application_icon', array('app_id' => $application['public_app_id'], 'resource' => 'small'), true); ?>" alt="<?PHP HTML::print(TEXT_APPLICATION_VIEW_APPLICATION_ICON_ALT); ?>">
                 </div>
                 <div class="content">
                     <div class="d-flex align-items-center">
                         <a class="d-flex align-items-center" style="text-decoration: none;" href="<?PHP DynamicalWeb::getRoute('manage_application', array('pub_id' => $application['public_app_id']), true); ?>">
                             <h6 class="product-name text-black"><?PHP HTML::print($application['name']); ?></h6>
                             <small class="time ml-3 d-none d-sm-block">
-                                <?PHP HTML::print(str_ireplace("%s", gmdate("F j, Y, g:i a", $application['creation_timestamp']), "Created at %s")); ?>
+                                <?PHP HTML::print(str_ireplace("%s", gmdate("F j, Y, g:i a", $application['creation_timestamp']), TEXT_APPLICATION_VIEW_CREATED_TIMESTAMP)); ?>
                             </small>
                         </a>
-
                         <?PHP
                             if($application['status'] !== ApplicationStatus::Suspended)
                             {
@@ -33,18 +32,18 @@ use IntellivoidAccounts\Abstracts\ApplicationStatus;
                                         <i class="mdi mdi-dots-vertical"></i>
                                     </button>
                                     <div class="dropdown-menu" aria-labelledby="dropdown-<?PHP HTML::print($application['last_updated_timestamp']); ?>">
-                                        <a class="dropdown-item" href="<?PHP DynamicalWeb::getRoute('manage_application', array('pub_id' => $application['public_app_id']), true); ?>">Manage Application</a>
+                                        <a class="dropdown-item" href="<?PHP DynamicalWeb::getRoute('manage_application', array('pub_id' => $application['public_app_id']), true); ?>"><?PHP HTML::print(TEXT_APPLICATION_VIEW_OPTIONS_MANAGE_APPLICATION); ?></a>
                                         <?PHP
                                         if($application['status'] == ApplicationStatus::Active)
                                         {
                                             ?>
-                                            <a class="dropdown-item" href="<?PHP DynamicalWeb::getRoute('manage_application', array('pub_id' => $application['public_app_id'], 'action' => 'disable-application', 'redirect' => 'applications'), true); ?>">Disable</a>
+                                            <a class="dropdown-item" href="<?PHP DynamicalWeb::getRoute('manage_application', array('pub_id' => $application['public_app_id'], 'action' => 'disable-application', 'redirect' => 'applications'), true); ?>"><?PHP HTML::print(TEXT_APPLICATION_VIEW_OPTIONS_DISABLE_APPLICATION); ?></a>
                                             <?PHP
                                         }
                                         if($application['status'] == ApplicationStatus::Disabled)
                                         {
                                             ?>
-                                            <a class="dropdown-item" href="<?PHP DynamicalWeb::getRoute('manage_application', array('pub_id' => $application['public_app_id'], 'action' => 'enable-application', 'redirect' => 'applications'), true); ?>">Enable</a>
+                                            <a class="dropdown-item" href="<?PHP DynamicalWeb::getRoute('manage_application', array('pub_id' => $application['public_app_id'], 'action' => 'enable-application', 'redirect' => 'applications'), true); ?>"><?PHP HTML::print(TEXT_APPLICATION_VIEW_OPTIONS_ENABLE_APPLICATION); ?></a>
                                             <?PHP
                                         }
                                         ?>
@@ -61,19 +60,19 @@ use IntellivoidAccounts\Abstracts\ApplicationStatus;
                                 {
                                     case ApplicationStatus::Active:
                                         HTML::print("<div class=\"badge badge-outline-success\">", false);
-                                        HTML::print('Active');
+                                        HTML::print(TEXT_APPLICATION_VIEW_STATE_ACTIVE);
                                         HTML::print("</div>", false);
                                         break;
 
                                     case ApplicationStatus::Disabled:
                                         HTML::print("<div class=\"badge badge-outline-danger\">", false);
-                                        HTML::print('Disabled');
+                                        HTML::print(TEXT_APPLICATION_VIEW_STATE_DISABLED);
                                         HTML::print("</div>", false);
                                         break;
 
                                     case ApplicationStatus::Suspended:
                                         HTML::print("<div class=\"badge badge-outline-warning\">", false);
-                                        HTML::print('Suspended');
+                                        HTML::print(TEXT_APPLICATION_VIEW_STATE_SUSPENDED);
                                         HTML::print("</div>", false);
                                         break;
                                 }
--- src/resources/pages/applications/scripts/ren.no_contents.php
+++ src/resources/pages/applications/scripts/ren.no_contents.php
@@ -1,3 +1,6 @@
+<?php
+    use DynamicalWeb\HTML;
+?>
 <div class="pt-4 pb-4">
-    <h4 class="text-muted text-center">No Applications</h4>
+    <h4 class="text-muted text-center"><?PHP HTML::print(TEXT_APPLICATION_VIEW_EMPTY); ?></h4>
 </div>
