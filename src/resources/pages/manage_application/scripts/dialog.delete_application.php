881a59ef8c04eddba7ad9cb35ad76a7714fe877a
netkas <netkas@intellivoid.info>
2020-02-14 13:08:29 -0300

    Created translation for 'manage_application'

--- src/resources/pages/manage_application/scripts/dialog.delete_application.php
+++ src/resources/pages/manage_application/scripts/dialog.delete_application.php
@@ -6,7 +6,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="delete-application-label">Delete Application</h5>
+                <h5 class="modal-title" id="delete-application-label"><?PHP HTML::print(TEXT_DELETE_APPLICATION_DIALOG_TITLE); ?></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">
                         <i class="mdi mdi-close"></i>
@@ -14,16 +14,12 @@
                 </button>
             </div>
             <div class="modal-body">
-                <p>
-                    Please confirm that you want to delete this Application, though the Application name is recoverable
-                    (if it's not taken by another user) the current access that this Application has will also be deleted.
-                    This action cannot be undone.
-                </p>
+                <p><?PHP HTML::print(TEXT_DELETE_APPLICATION_DIALOG_BODY); ?></p>
             </div>
             <div class="modal-footer">
                 <?PHP $Href = DynamicalWeb::getRoute('manage_application', array('pub_id' => $_GET['pub_id'], 'action' => 'delete-application')); ?>
-                <button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-danger" onclick="location.href='<?PHP HTML::print($Href); ?>';">Delete</button>
+                <button type="button" class="btn btn-light" data-dismiss="modal"><?PHP HTML::print(TEXT_DELETE_APPLICATION_DIALOG_CANCEL_BUTTON); ?></button>
+                <button type="button" class="btn btn-danger" onclick="location.href='<?PHP HTML::print($Href); ?>';"><?PHP HTML::print(TEXT_DELETE_APPLICATION_DIALOG_DELETE_BUTTON); ?></button>
             </div>
         </div>
     </div>
