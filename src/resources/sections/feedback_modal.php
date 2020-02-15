3c51ea273b690cdf3ed17139dc7cb8ff4511a56c
netkas <netkas@intellivoid.info>
2020-02-15 17:07:55 -0300

    Added translation to section 'feedback_modal'

--- src/resources/sections/feedback_modal.php
+++ src/resources/sections/feedback_modal.php
@@ -1,12 +1,13 @@
 <?PHP
     use DynamicalWeb\DynamicalWeb;
+    use DynamicalWeb\HTML;
 ?>
 <div class="modal fade" id="feedback_dialog" tabindex="-1" role="dialog" aria-labelledby="fdl" aria-hidden="true" style="display: none;">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <form action="<?PHP DynamicalWeb::getRoute('index', array('action' => 'submit_feedback'), true) ?>" method="POST">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="fdl">Send Feedback</h5>
+                    <h5 class="modal-title" id="fdl"><?PHP HTML::print(TEXT_FEEDBACK_DIALOG_TITLE); ?></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">
                             <i class="mdi mdi-close"></i>
@@ -15,13 +16,13 @@
                 </div>
                 <div class="modal-body">
                     <div class="form-group">
-                        <label for="feedback_message">Message</label>
-                        <textarea name="feedback_message" id="feedback_message" class="form-control" rows="12" placeholder="Write your message here and we will get back to you as soon as possible via Email"></textarea>
+                        <label for="feedback_message"><?PHP HTML::print(TEXT_FEEDBACK_DIALOG_MESSAGE_LABEL); ?></label>
+                        <textarea name="feedback_message" id="feedback_message" class="form-control" rows="12" placeholder="<?PHP HTML::print(TEXT_FEEDBACK_DIALOG_MESSAGE_PLACEHOLDER); ?>"></textarea>
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-light" data-dismiss="modal">Cancel</button>
-                    <input type="submit" class="btn btn-success" value="Send">
+                    <button type="button" class="btn btn-light" data-dismiss="modal"><?PHP HTML::print(TEXT_FEEDBACK_DIALOG_CANCEL_BUTTON); ?></button>
+                    <input type="submit" class="btn btn-success" value="<?PHP HTML::print(TEXT_FEEDBACK_DIALOG_SUBMIT_BUTTON); ?>">
                 </div>
             </form>
         </div>
