375f1a82a0e7da4304561c82e60333ad0769fafb
netkas <netkas@intellivoid.info>
2020-03-31 14:47:23 -0300

    Corrected AutoConfig

--- src/resources/libraries/Support/Support.php
+++ src/resources/libraries/Support/Support.php
@@ -34,11 +34,6 @@
         include_once(__DIR__ . DIRECTORY_SEPARATOR . 'acm' . DIRECTORY_SEPARATOR . 'acm.php');
     }
 
-    if(class_exists('msqg\msqg') == false)
-    {
-        include_once(__DIR__ . DIRECTORY_SEPARATOR . 'msqg' . DIRECTORY_SEPARATOR . 'msqg.php');
-    }
-
     include(__DIR__ . DIRECTORY_SEPARATOR . 'AutoConfig.php');
 
     /**
