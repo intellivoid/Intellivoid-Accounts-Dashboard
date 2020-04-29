c79f6e4fa412c5cb81c5dd31d2eaa2d91503114b
netkas <netkas@intellivoid.info>
2020-04-29 18:43:04 -0400

    mayax_dashboard_headers to resources->sections

--- src/resources/pages/blank/contents.php
+++ src/resources/pages/blank/contents.php
@@ -1,38 +1,11 @@
+<?php
+    use DynamicalWeb\HTML;
+?>
 <!DOCTYPE html>
-
 <html class="loading" lang="en" data-textdirection="ltr">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-        <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-        <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
-        <meta name="author" content="PIXINVENT">
-        <title>Fixed Layout - Vuexy - Bootstrap HTML admin template</title>
-        <link rel="apple-touch-icon" href="../../../app-assets/images/ico/apple-icon-120.png">
-        <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/favicon.ico">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
-
-        <!-- BEGIN: Vendor CSS-->
-        <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/vendors.min.css">
-        <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/ui/prism.min.css">
-        <!-- END: Vendor CSS-->
-
-        <!-- BEGIN: Theme CSS-->
-        <link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="../../../app-assets/css/bootstrap-extended.css">
-        <link rel="stylesheet" type="text/css" href="../../../app-assets/css/colors.css">
-        <link rel="stylesheet" type="text/css" href="../../../app-assets/css/components.css">
-        <link rel="stylesheet" type="text/css" href="../../../app-assets/css/themes/dark-layout.css">
-        <link rel="stylesheet" type="text/css" href="../../../app-assets/css/themes/semi-dark-layout.css">
-
-        <!-- BEGIN: Page CSS-->
-        <link rel="stylesheet" type="text/css" href="../../../app-assets/css/core/menu/menu-types/horizontal-menu.css">
-        <!-- END: Page CSS-->
-
-        <!-- BEGIN: Custom CSS-->
-        <link rel="stylesheet" type="text/css" href="../../../assets/css/style.css">
-        <!-- END: Custom CSS-->
+        <?PHP HTML::importSection('mayax_dashboard_headers'); ?>
+        <title>Blank Page</title>
     </head>
 <!-- END: Head-->
 
