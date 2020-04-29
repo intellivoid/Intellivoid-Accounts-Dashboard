8a31dc957950ea9e2341fb170060161fae263d59
netkas <netkas@intellivoid.info>
2020-04-29 19:19:12 -0400

    mayax_dashboard_horizontal_menu to resources->sections

--- src/resources/pages/blank/contents.php
+++ src/resources/pages/blank/contents.php
@@ -11,45 +11,8 @@
 
         <?PHP HTML::importSection('mayax_dashboard_bhelper'); ?>
         <?PHP HTML::importSection('mayax_dashboard_nav'); ?>
+        <?PHP HTML::importSection('mayax_dashboard_horizontal_menu'); ?>
 
-        <div class="horizontal-menu-wrapper">
-            <div class="header-navbar navbar-expand-sm navbar navbar-horizontal fixed-top navbar-light navbar-without-dd-arrow navbar-shadow menu-border" role="navigation" data-menu="menu-wrapper">
-                <div class="navbar-header">
-                    <ul class="nav navbar-nav flex-row">
-                        <li class="nav-item mr-auto"><a class="navbar-brand" href="../../../html/ltr/horizontal-menu-template/index.html">
-                                <div class="brand-logo"></div>
-                                <h2 class="brand-text mb-0">Vuexy</h2>
-                            </a></li>
-                        <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary" data-ticon="icon-disc"></i></a></li>
-                    </ul>
-                </div>
-                <!-- Horizontal menu content-->
-                <div class="navbar-container main-menu-content" data-menu="menu-container">
-                    <!-- include ../../../includes/mixins-->
-                    <ul class="nav navbar-nav" id="main-menu-navigation" data-menu="menu-navigation">
-                        <li class="nav-item"><a class="nav-link" href="../../../html/ltr/horizontal-menu-template/index.html"><i class="feather icon-home"></i><span data-i18n="Dashboard">Dashboard</span></a></li>
-                        <li class="dropdown nav-item" data-menu="dropdown"><a class="dropdown-toggle nav-link" href="#" data-toggle="dropdown"><i class="feather icon-zap"></i><span data-i18n="Starter kit">Starter kit</span></a>
-                            <ul class="dropdown-menu">
-                                <li data-menu=""><a class="dropdown-item" href="sk-layout-2-columns.html" data-toggle="dropdown" data-i18n="2 columns">2 columns</a>
-                                </li>
-                                <li data-menu=""><a class="dropdown-item" href="sk-layout-fixed-navbar.html" data-toggle="dropdown" data-i18n="Fixed navbar">Fixed navbar</a>
-                                </li>
-                                <li data-menu=""><a class="dropdown-item" href="sk-layout-floating-navbar.html" data-toggle="dropdown" data-i18n="Floating navbar">Floating navbar</a>
-                                </li>
-                                <li class="active" data-menu=""><a class="dropdown-item" href="sk-layout-fixed.html" data-toggle="dropdown" data-i18n="Fixed layout">Fixed layout</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="https://pixinvent.ticksy.com/"><i class="feather icon-life-buoy"></i><span>Raise Support</span></a></li>
-                        <li class="nav-item"><a class="nav-link" href="https://pixinvent.com/demo/vuexy-html-bootstrap-admin-template/documentation"><i class="feather icon-folder"></i><span>Documentation</span></a></li>
-                    </ul>
-                </div>
-                <!-- /horizontal menu content-->
-            </div>
-        </div>
-        <!-- END: Main Menu-->
-
-        <!-- BEGIN: Content-->
         <div class="app-content content">
             <div class="content-overlay"></div>
             <div class="header-navbar-shadow"></div>
---
+++ src/resources/sections/mayax_dashboard_horizontal_menu.php
@@ -0,0 +1,62 @@
+<div class="horizontal-menu-wrapper">
+    <div class="header-navbar navbar-expand-sm navbar navbar-horizontal fixed-top navbar-light navbar-without-dd-arrow navbar-shadow menu-border" role="navigation" data-menu="menu-wrapper">
+        <div class="navbar-header">
+            <ul class="nav navbar-nav flex-row">
+                <li class="nav-item mr-auto">
+                    <a class="navbar-brand" href="#">
+                        <div class="brand-logo"></div>
+                        <h2 class="brand-text mb-0">Vuexy</h2>
+                    </a>
+                </li>
+                <li class="nav-item nav-toggle">
+                    <a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse">
+                        <i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i>
+                        <i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary" data-ticon="icon-disc"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="navbar-container main-menu-content" data-menu="menu-container">
+            <ul class="nav navbar-nav" id="main-menu-navigation" data-menu="menu-navigation">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="feather icon-home"></i>
+                        <span data-i18n="Dashboard">Dashboard</span>
+                    </a>
+                </li>
+                <li class="dropdown nav-item" data-menu="dropdown">
+                    <a class="dropdown-toggle nav-link" href="#" data-toggle="dropdown">
+                        <i class="feather icon-zap"></i>
+                        <span data-i18n="Starter kit">Starter kit</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li data-menu="">
+                            <a class="dropdown-item" href="#" data-toggle="dropdown" data-i18n="2 columns">2 columns</a>
+                        </li>
+                        <li data-menu="">
+                            <a class="dropdown-item" href="#" data-toggle="dropdown" data-i18n="Fixed navbar">Fixed navbar</a>
+                        </li>
+                        <li data-menu="">
+                            <a class="dropdown-item" href="#" data-toggle="dropdown" data-i18n="Floating navbar">Floating navbar</a>
+                        </li>
+                        <li class="active" data-menu="">
+                            <a class="dropdown-item" href="#" data-toggle="dropdown" data-i18n="Fixed layout">Fixed layout</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://pixinvent.ticksy.com/">
+                        <i class="feather icon-life-buoy"></i>
+                        <span>Raise Support</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://pixinvent.com/demo/vuexy-html-bootstrap-admin-template/documentation">
+                        <i class="feather icon-folder"></i>
+                        <span>Documentation</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
