ea24a439e559932e0077486c87f1a9862167f94d
netkas <netkas@intellivoid.info>
2020-04-29 19:26:13 -0400

    Content cleanup

--- src/resources/pages/blank/contents.php
+++ src/resources/pages/blank/contents.php
@@ -16,99 +16,15 @@
         <div class="app-content content">
             <?PHP HTML::importSection('mayax_dashboard_chelper'); ?>
             <div class="content-wrapper">
-                <div class="content-header row">
-                    <div class="content-header-left col-md-9 col-12 mb-2">
-                        <div class="row breadcrumbs-top">
-                            <div class="col-12">
-                                <h2 class="content-header-title float-left mb-0">Fixed Layout</h2>
-                                <div class="breadcrumb-wrapper col-12">
-                                    <ol class="breadcrumb">
-                                        <li class="breadcrumb-item"><a href="sk-layout-2-columns.html">Home</a>
-                                        </li>
-                                        <li class="breadcrumb-item"><a href="#">Starter Kit</a>
-                                        </li>
-                                        <li class="breadcrumb-item active">Fixed Layout
-                                        </li>
-                                    </ol>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="content-header-right text-md-right col-md-3 col-12 d-md-block d-none">
-                        <div class="form-group breadcrum-right">
-                            <div class="dropdown">
-                                <button class="btn-icon btn btn-primary btn-round btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="feather icon-settings"></i></button>
-                                <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#">Chat</a><a class="dropdown-item" href="#">Email</a><a class="dropdown-item" href="#">Calendar</a></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div class="content-body">
-                    <!-- Description -->
                     <section id="description" class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Description</h4>
+                            <h4 class="card-title">Lorem ipsum</h4>
                         </div>
                         <div class="card-content">
                             <div class="card-body">
                                 <div class="card-text">
-                                    <p>The fixed layout has a fixed navbar, navigation menu and footer only content section is scrollable to user. In this page you can experience it. Fixed layout provide seamless UI on different screens.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <!--/ Description -->
-                    <!-- CSS Classes -->
-                    <section id="css-classes" class="card">
-                        <div class="card-header">
-                            <h4 class="card-title">CSS Classes</h4>
-                        </div>
-                        <div class="card-content">
-                            <div class="card-body">
-                                <div class="card-text">
-                                    <p>This table contains all classes related to the fixed layout. This is a custom layout classes for fixed layout page requirements.</p>
-                                    <p>All these options can be set via following classes:</p>
-                                    <div class="table-responsive">
-                                        <table class="table table-hover">
-                                            <thead>
-                                            <tr>
-                                                <th>Classes</th>
-                                                <th>Description</th>
-                                            </tr>
-                                            </thead>
-                                            <tbody>
-                                            <tr>
-                                                <th scope="row"><code>.navbar-sticky</code></th>
-                                                <td>To set navbar fixed you need to add <code>navbar-sticky</code> class in <code>&lt;body&gt;</code> tag.</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row"><code>.fixed-top</code></th>
-                                                <td>To set navbar fixed you need to add <code>fixed-top</code> class in <code>&lt;nav&gt;</code> tag.</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row"><code>.menu-fixed</code></th>
-                                                <td>To set the main navigation fixed on page <code>menu-fixed</code> class needs to add in navigation wrapper.</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row"><code>.fixed-footer</code></th>
-                                                <td>To set the main footer fixed on page <code>fixed-footer</code> class needs to add in <code>&lt;body&gt;</code> tag.</td>
-                                            </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <section id="html-markup" class="card">
-                        <div class="card-header">
-                            <h4 class="card-title">HTML Markup</h4>
-                        </div>
-                        <div class="card-content">
-                            <div class="card-body">
-                                <div class="card-text">
-                                    <p>This section contains HTML Markup to create fixed layout. This markup define where to add css classes to make navbar, navigation & footer fixed.</p>
-                                    <p>Vuexy has a ready to use starter kit, you can use this layout directly by using the starter kit pages from the <code>vuexy-html-bootstrap-admin-template/starter-kit</code> folder.</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                                 </div>
                             </div>
                         </div>
