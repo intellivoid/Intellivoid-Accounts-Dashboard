a5218746402e66188bc35d3dadfa6fe2e25950aa
netkas <netkas@intellivoid.info>
2020-04-29 22:04:56 -0400

    Updated change_language_modal.go

--- src/resources/sections/change_language_modal.php
+++ src/resources/sections/change_language_modal.php
@@ -1,40 +1,43 @@
 <?PHP
     use DynamicalWeb\DynamicalWeb;
 ?>
-<div class="modal fade" id="change-language-dialog" tabindex="-1" role="dialog" aria-labelledby="crd" aria-hidden="true" style="display: none;">
-    <div class="modal-dialog" role="document">
+<div class="modal fade text-left" id="change-language-dialog" tabindex="-1" role="dialog" aria-labelledby="crd" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="crd">Change Language</h5>
+                <h4 class="modal-title" id="crd">Change Language</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">
-                        <i class="mdi mdi-close"></i>
+                        <i class="feather icon-x-circle"></i>
                     </span>
                 </button>
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-4 mt-3">
+                    <div class="col-4">
                         <a href="<?PHP DynamicalWeb::getRoute('change_language', array('language' => 'en', 'cache' => hash('sha256', time())), true); ?>">
                             <i class="flag-icon flag-icon-gb" title="gb" id="gb"></i> English (UK)
                         </a>
                     </div>
-                    <div class="col-4 mt-3">
+                    <div class="col-4">
                         <a href="<?PHP DynamicalWeb::getRoute('change_language', array('language' => 'zh', 'cache' => hash('sha256', time())), true); ?>">
                             <i class="flag-icon flag-icon-cn" title="cn" id="cn"></i> 中文
                         </a>
                     </div>
-                    <div class="col-4 mt-3">
+                    <div class="col-4">
                         <a href="<?PHP DynamicalWeb::getRoute('change_language', array('language' => 'es', 'cache' => hash('sha256', time())), true); ?>">
                             <i class="flag-icon flag-icon-cl" title="es" id="es"></i> Español
                         </a>
                     </div>
-                    <div class="col-4 mt-3">
+                    <div class="col-4 mt-1">
                         <a href="<?PHP DynamicalWeb::getRoute('change_language', array('language' => 'tr', 'cache' => hash('sha256', time())), true); ?>">
                             <i class="flag-icon flag-icon-tr" title="tr" id="tr"></i> Türk
                         </a>
                     </div>
                 </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary waves-effect waves-light" data-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
--- src/resources/sections/mayax_dashboard_ehelper.php
+++ src/resources/sections/mayax_dashboard_ehelper.php
@@ -1,6 +1,7 @@
 <?php
     use DynamicalWeb\HTML;
+
+    HTML::importSection('change_language_modal');
 ?>
-<?php HTML::importSection('change_language_modal'); ?>
 <div class="sidenav-overlay"></div>
 <div class="drag-target"></div>
