a4af19a7180121c4f8a1f2a44a7c290e949c94b3
netkas <netkas@intellivoid.info>
2020-05-02 16:14:27 -0400

    Updated authentication_footer.go

--- src/resources/sections/authentication_footer.php
+++ src/resources/sections/authentication_footer.php
@@ -1,5 +1,7 @@
 <?php
     use DynamicalWeb\DynamicalWeb;
+use DynamicalWeb\HTML;
+
 ?>
 <span class="float-left">
     <a data-toggle="modal" data-target="#change-language-dialog" href="#">
@@ -7,10 +9,12 @@
     </a>
 </span>
 <span class="float-right">
-    <a href="<?PHP DynamicalWeb::getRoute('tos', [], true); ?>" class="card-link">Terms of Service
+    <a href="<?PHP DynamicalWeb::getRoute('tos', [], true); ?>" class="card-link">
+        <?PHP HTML::print(TEXT_AUTH_FOOTER_TOS_LINK); ?>
         <i class="fa fa-angle-right"></i>
     </a>
-    <a href="<?PHP DynamicalWeb::getRoute('privacy', [], true); ?>" class="card-link">Privacy
+    <a href="<?PHP DynamicalWeb::getRoute('privacy', [], true); ?>" class="card-link">
+        <?PHP HTML::print(TEXT_AUTH_FOOTER_PRIVACY_LINK); ?>
         <i class="fa fa-angle-right"></i>
     </a>
 </span>
