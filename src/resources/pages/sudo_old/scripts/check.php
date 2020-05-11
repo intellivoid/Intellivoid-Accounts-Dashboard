27ea0addfc40302c3408ea770f95931ce1c5ed37
netkas <netkas@intellivoid.info>
2020-05-10 20:30:36 -0400

    Removed unused assets

--- src/resources/pages/sudo_old/scripts/check.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    use DynamicalWeb\Actions;
-    use DynamicalWeb\DynamicalWeb;
-
-    if(WEB_SUDO_MODE == true)
-    {
-        Actions::redirect(DynamicalWeb::getRoute('index'));
-    }
