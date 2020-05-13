ab0648e78ed1063a5125cd87699dbfa433b55a47
netkas <netkas@intellivoid.info>
2020-05-13 15:17:05 -0400

    Added recent transaction history in index

--- src/resources/pages/index/contents.php
+++ src/resources/pages/index/contents.php
@@ -97,53 +97,17 @@
                                             <div class="row pb-50">
                                                 <div class="col-lg-6 col-12 d-flex justify-content-between flex-column order-lg-1 order-2 mt-lg-0 mt-2">
                                                     <div>
-                                                        <h2 class="text-bold-700 mb-25">$0</h2>
-                                                        <p class="text-bold-500 mb-75">Account Balance</p>
+                                                        <h2 class="text-bold-700 mb-25">$<?PHP HTML::print($Account->Configuration->Balance); ?> USD</h2>
+                                                        <p class="text-bold-500 mb-75"><?PHP HTML::print(TEXT_ACCOUNT_BALANCE_TEXT); ?></p>
                                                     </div>
-                                                    <a href="#" class="btn btn-primary shadow">View Details
+                                                    <a href="<?PHP DynamicalWeb::getRoute('account_balance', array(), true); ?>" class="btn btn-primary shadow"><?PHP HTML::print(TEXT_ACCOUNT_BALANCE_MANAGE_BUTTON); ?>
                                                         <i class="feather icon-chevrons-right"></i>
                                                     </a>
                                                 </div>
-                                                <div class="col-lg-6 col-12 d-flex justify-content-between flex-column text-right order-lg-2 order-1">
-                                                    <div class="dropdown chart-dropdown">
-                                                        <button class="btn btn-sm border-0 dropdown-toggle p-0" type="button" id="dropdownItem5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                            Last 7 Days
-                                                        </button>
-                                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownItem5">
-                                                            <a class="dropdown-item" href="#">Last 28 Days</a>
-                                                            <a class="dropdown-item" href="#">Last Month</a>
-                                                            <a class="dropdown-item" href="#">Last Year</a>
-                                                        </div>
-                                                    </div>
-                                                    <div id="avg-session-chart"></div>
-                                                </div>
                                             </div>
                                             <hr />
-                                            <div class="row avg-sessions pt-50">
-                                                <div class="col-6">
-                                                    <p class="mb-0">Goal: $100000</p>
-                                                    <div class="progress progress-bar-primary mt-25">
-                                                        <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="50" aria-valuemax="100" style="width:50%"></div>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6">
-                                                    <p class="mb-0">Users: 100K</p>
-                                                    <div class="progress progress-bar-warning mt-25">
-                                                        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="60" aria-valuemax="100" style="width:60%"></div>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6">
-                                                    <p class="mb-0">Retention: 90%</p>
-                                                    <div class="progress progress-bar-danger mt-25">
-                                                        <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="70" aria-valuemax="100" style="width:70%"></div>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6">
-                                                    <p class="mb-0">Duration: 1yr</p>
-                                                    <div class="progress progress-bar-success mt-25">
-                                                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="90" aria-valuemax="100" style="width:90%"></div>
-                                                    </div>
-                                                </div>
+                                            <div class="row">
+                                                <?PHP HTML::importScript('recent_transactions'); ?>
                                             </div>
                                         </div>
                                     </div>
