360eea3849991340e960cb6b6bf07ebb5463fb68
netkas <netkas@intellivoid.info>
2020-05-22 23:44:17 -0400

    Updated 'index'

--- src/resources/pages/index/scripts/recent_transactions.php
+++ src/resources/pages/index/scripts/recent_transactions.php
@@ -160,7 +160,7 @@
             <?PHP
         }
         ?>
-        <button class="btn btn-block bg-gradient-danger mt-2 mx-2" onclick="location.href='<?PHP DynamicalWeb::getRoute('transaction_history', [], true); ?>';">
+        <button class="btn btn-block bg-gradient-danger mt-2 mx-2" onclick="location.href='<?PHP DynamicalWeb::getRoute('finance_transactions', [], true); ?>';">
             <?PHP HTML::print(TEXT_VIEW_TRANSACTION_HISTORY); ?>
         </button>
         <?php
