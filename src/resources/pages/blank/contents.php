174c276f1bab27f4547bc8db75264caffc423525
netkas <netkas@intellivoid.info>
2020-05-23 02:05:53 -0400

    Added 'generic_nav'

--- src/resources/pages/blank/contents.php
+++ src/resources/pages/blank/contents.php
@@ -1,34 +1,31 @@
 <?php
+
+    /** @noinspection PhpUndefinedConstantInspection */
+
+    use DynamicalWeb\DynamicalWeb;
     use DynamicalWeb\HTML;
+    use DynamicalWeb\Runtime;
+    use IntellivoidAccounts\Abstracts\SearchMethods\AccountSearchMethod;
+    use IntellivoidAccounts\Abstracts\SearchMethods\ApplicationSearchMethod;
+    use IntellivoidAccounts\IntellivoidAccounts;
+    use IntellivoidAccounts\Objects\TransactionRecord;
 ?>
 <!DOCTYPE html>
 <html class="loading" lang="<?PHP HTML::print(APP_LANGUAGE_ISO_639); ?>" data-textdirection="ltr">
     <head>
         <?PHP HTML::importSection('main_headers'); ?>
-        <title>Blank Page</title>
+        <title>Blank</title>
     </head>
     <body class="horizontal-layout horizontal-menu 2-columns navbar-sticky fixed-footer" data-open="hover" data-menu="horizontal-menu" data-col="2-columns">
 
         <?PHP HTML::importSection('main_bhelper'); ?>
-        <?PHP HTML::importSection('main_nav'); ?>
-        <?PHP HTML::importSection('main_horizontal_menu'); ?>
+        <?PHP HTML::importSection('generic_nav'); ?>
 
-        <div class="app-content content">
+        <div class="app-content content mb-0">
             <?PHP HTML::importSection('main_chelper'); ?>
             <div class="content-wrapper">
                 <div class="content-body">
-                    <section id="description" class="card">
-                        <div class="card-header">
-                            <h4 class="card-title">Lorem ipsum</h4>
-                        </div>
-                        <div class="card-content">
-                            <div class="card-body">
-                                <div class="card-text">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+
 
                 </div>
             </div>
---
+++ src/resources/sections/generic_nav.php
@@ -0,0 +1,23 @@
+<?php
+    use DynamicalWeb\DynamicalWeb;
+    use DynamicalWeb\HTML;
+
+?>
+<nav class="header-navbar navbar-expand-lg navbar navbar-with-menu navbar-light navbar-shadow">
+    <div class="navbar-wrapper">
+        <div class="navbar-container content">
+            <div class="navbar-collapse" id="navbar-mobile">
+                <div class="mr-auto float-left bookmark-wrapper d-flex align-items-center">
+                    <img src="/assets/images/logo_2.svg" alt="Intellivoid Accounts Brand" style="width: 130px; height: 30px;" class="img-fluid my-2">
+                </div>
+                <ul class="nav navbar-nav float-right mr-2">
+                    <li class="nav-item d-block">
+                        <a class="nav-link" href="#">
+                            <i class="ficon feather icon-home"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
