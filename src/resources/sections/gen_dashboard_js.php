68d7bb38da54dc98327a02ee49a3b7f86b83255a
netkas <netkas@intellivoid.info>
2020-05-23 15:13:07 -0400

    Removed 'gen_dashboard_js'

--- src/resources/sections/gen_dashboard_js.php
+++
@@ -1,6 +0,0 @@
-<script src="/assets/vendors/js/vendor.bundle.base.js"></script>
-<script src="/assets/vendors/js/vendor.bundle.addons.js"></script>
-<script src="/assets/js/shared/off-canvas.js"></script>
-<script src="/assets/js/shared/hoverable-collapse.js"></script>
-<script src="/assets/js/shared/misc.js"></script>
-<script src="/assets/js/navigation.js"></script>
