304c1018d406d70c9869232bc8b841b8b596fb26
netkas <netkas@intellivoid.info>
2020-05-23 00:19:41 -0400

    Corrected 'no_items' view in 'finance_subscriptions'

--- src/resources/pages/finance_subscriptions/scripts/ren.no_contents.php
+++ src/resources/pages/finance_subscriptions/scripts/ren.no_contents.php
@@ -3,9 +3,6 @@
 ?>
 <div class="d-flex flex-column justify-content-center align-items-center" style="height:50vh;">
     <div class="p-2 my-flex-item">
-        <img src="/assets/images/sadboi.svg" class="img-fluid img-md" alt="No items icon"/>
-    </div>
-    <div class="p-2 my-flex-item">
-        <h6 class="text-muted"><?PHP HTML::print(TEXT_NO_ITEMS); ?></h6>
+        <h3 class="text-muted"><?PHP HTML::print(TEXT_NO_ITEMS); ?></h3>
     </div>
 </div>
