c7796e9fac1e7b99aa807953299a5a10059439fa
netkas <netkas@intellivoid.info>
2020-05-25 21:28:41 -0400

    Updated 'Delete Application Dialog'

--- src/resources/pages/manage_application/scripts/dialog.delete_application.php
+++ src/resources/pages/manage_application/scripts/dialog.delete_application.php
@@ -2,19 +2,14 @@
     use DynamicalWeb\DynamicalWeb;
     use DynamicalWeb\HTML;
 ?>
-<div class="modal fade" id="delete-application" tabindex="-1" role="dialog" aria-labelledby="delete-application-label" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+<div class="modal fade text-left" id="delete-application" tabindex="-1" role="dialog" aria-labelledby="delete-application-label" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="delete-application-label"><?PHP HTML::print(TEXT_DELETE_APPLICATION_DIALOG_TITLE); ?></h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">
-                        <i class="mdi mdi-close"></i>
-                    </span>
-                </button>
+                <h4 class="modal-title" id="delete-application-label"><?PHP HTML::print(TEXT_DELETE_APPLICATION_DIALOG_TITLE); ?></h4>
             </div>
             <div class="modal-body">
-                <p><?PHP HTML::print(TEXT_DELETE_APPLICATION_DIALOG_BODY); ?></p>
+                <?PHP HTML::print(TEXT_DELETE_APPLICATION_DIALOG_BODY); ?>
             </div>
             <div class="modal-footer">
                 <?PHP $Href = DynamicalWeb::getRoute('manage_application', array('pub_id' => $_GET['pub_id'], 'action' => 'delete_application')); ?>
